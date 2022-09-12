f4b347fd7e2da3ed086028b40dd0dd84b45a6e4c
11AlexanderB <bezhenar93@internet.ru>
2022-09-12 09:33:52 +0300

    FillArray

---
+++ 1task/Program.cs
@@ -0,0 +1,20 @@
+﻿void FillArray()
+{
+    Console.WriteLine("Введите количество строк: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Вывод массива на экран: ");
+    double [,] Array = new double [m,n];
+
+    for(int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            Array[i,j] = Math.Round( new Random().Next(-10, 10) + new Random().NextDouble(), 1 );
+            Console.Write($"{Array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray();
